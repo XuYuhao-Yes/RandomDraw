b1804563d2be5abd07b0501d07d6a592af3dadcd
XuYuhao <reddragons2021@outlook.com>
2023-09-08 23:29:49 +0800

    添加项目基础框架

---
+++ screen.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+class Screen:
+    def __init__(self):
+        self.screen = Tk()
+        self.screen.title("RandomDraw")
+
+    def run(self):
+        self.screen.mainloop()
